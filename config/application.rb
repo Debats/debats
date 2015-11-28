3f87c3f39d563b8026b8030539f0c7a6c6f31fed
Jalil Arfaoui <jalil@arfaoui.net>
2015-11-28 16:34:28 +0100

    Added vendor assets precompilation

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,5 @@ module Debats
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    # Precompile vendors assets
-    config.assets.paths << Rails.root.join("vendor", "assets")
   end
 end
