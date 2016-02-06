381facf3feb7ddd4952ba2b3ff7ecbc2bf6569e3
Nadia Arfaoui <nadia@arfaoui.net>
2016-02-06 23:44:58 +0100

    Definition of auth_to_update in subjects_controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -59,7 +59,14 @@ class SubjectsController < ApplicationController
     end
 
     def auth_to_update
-
+      if !current_user
+        store_location
+        flash[:danger] = "Vous devez être identifié pour éditer un sujet"
+        redirect_to login_url
+      elsif ! allowed_to :edit_subject
+        flash[:danger] = "Vous n'avez pas assez de réputation pour éditer un sujet"
+        redirect_to(Subject.find(params[:id]))
+      end
     end
 
     def auth_to_destroy
