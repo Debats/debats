1ecb6c0c9ffcac0491e2c60fa742c53afc319242
Marwann <marwann@gmail.com>
2016-02-06 15:06:35 +0100

    Revert "Création homepage"
    This reverts commit 61d8b873814c70eb984cd7b0ef25854e4087d5c4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-    root                  'home#index'
+  root                  'subjects#index'
 
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
