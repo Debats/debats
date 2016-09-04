f7e57d2551345e45d27bdbab9300bce6076660e7
Nadia Arfaoui <nadia@arfaoui.net>
2016-09-04 17:48:29 +0200

    added public figure (in futur public figure page) and associated subjects

--- src/components/PublicFigureAvatar/index.jsx
+++ src/components/PublicFigureAvatar/index.jsx
@@ -5,10 +5,11 @@ import cssModules from 'react-css-modules';
 const PublicFigureAvatar = ({ publicFigure }) => {
     if (!publicFigure.picture) return null;
 
-    return <div styleName="wrapper" style={`background: url('${publicFigure.picture.url}')`}></div>;
+    return <div styleName="wrapper" style={{ backgroundUrl: publicFigure.picture.url }}></div>;
 };
 PublicFigureAvatar.propTypes = {
     publicFigure: PropTypes.object.isRequired,
 };
 
 export default cssModules(PublicFigureAvatar, styles);
+
