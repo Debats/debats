6fbd5b3430ccfdc28cd4e972088409267ee9e757
ato <jvincent@student.42.fr>
2016-10-24 22:53:34 +0200

    minimal webpack refacto

---
+++ webpack/loaders.js
@@ -0,0 +1,40 @@
+const CONSTANTS = require('../webpack/constants');
+const APP_PATH = CONSTANTS.APP_PATH;
+
+const loaders = [
+    {
+        test: /\.css$/,
+        loader: 'style-loader'
+        + '!css-loader'
+        + '?modules&localIdentName=[name]__[local]___[hash:base64:5]'
+        + '&importLoaders=1'
+        + '!postcss-loader',
+        exclude: /node_modules/,
+    },
+    {
+        test: /\.(png|svg|gif|jpeg|jpg)$/,
+        include: APP_PATH,
+        loader: 'file?name=images/[name].[ext]',
+    },
+    {
+        test: /\.(eot|woff|ttf)$/,
+        include: APP_PATH,
+        loader: 'file-loader?name=fonts/[name].[ext]',
+    },
+    {
+        test: /\.(svg|woff2?)$/,
+        exclude: APP_PATH,
+        loader: 'url?limit=10000',
+    },
+    {
+        test: /\.(ttf|eot)$/,
+        exclude: APP_PATH,
+        loader: 'file',
+    },
+    {
+        test: /\.json$/,
+        loader: 'json',
+    },
+];
+
+module.exports = loaders;
--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -2,11 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CompressionPlugin = require('compression-webpack-plugin')
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const postcssProcessors = require('./postcss-processors');
-
+const loaders = require('./loaders');
 
 const CONSTANTS = require('./constants');
 const APP_ROOT = CONSTANTS.APP_ROOT;
@@ -14,16 +12,16 @@ const APP_PATH = CONSTANTS.APP_PATH;
 const APP_NAME = CONSTANTS.APP_NAME;
 const SRC_FOLDER = CONSTANTS.SRC_FOLDER;
 
-
+// Set-up common plugins
 const plugins = [];
-plugins.push(new webpack.NoErrorsPlugin());                                 // No assets emitted with errors
-plugins.push(new ExtractTextPlugin('[name].[contenthash].css', { allChunks: false }));  // Extract CSS to external style.css file
+
+plugins.push(new webpack.NoErrorsPlugin()); // No assets emitted with errors
+plugins.push(new ExtractTextPlugin('[name].[contenthash].css', { allChunks: false })); // Extract CSS to external style.css file
 plugins.push(new HtmlWebpackPlugin({
     template: `${SRC_FOLDER}/index.html`,
     inject: true,
     filename: 'index.html',
 }));
-
 
 module.exports = {
     resolve: {
@@ -37,12 +35,6 @@ module.exports = {
     output: {
         filename: `${APP_NAME}.[name].[hash].js`,
     },
-    externals: {
-        TweenLite: 'TweenLite',
-    },
-    node: {
-        fs: 'empty',
-    },
     plugins,
     module: {
         loaders: [
@@ -51,40 +43,14 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
             },
-            {
-                test: /\.css$/,
-                loader: 'style-loader'
-                + '!css-loader'
-                    + '?modules&localIdentName=[name]__[local]___[hash:base64:5]'
-                    + '&importLoaders=1'
-                + '!postcss-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|svg|gif|jpeg|jpg)$/,
-                include: APP_PATH,
-                loader: 'file?name=images/[name].[ext]',
-            },
-            {
-                test: /\.(eot|woff|ttf)$/,
-                include: APP_PATH,
-                loader: 'file-loader?name=fonts/[name].[ext]',
-            },
-            {
-                test: /\.(svg|woff2?)$/,
-                exclude: APP_PATH,
-                loader: "url?limit=10000"
-            },
-            {
-                test: /\.(ttf|eot)$/,
-                exclude: APP_PATH,
-                loader: "file"
-            },
-            {
-                test: /\.json$/,
-                loader: 'json',
-            },
+            ...loaders,
         ],
+    },
+    externals: {
+        TweenLite: 'TweenLite',
+    },
+    node: {
+        fs: 'empty',
     },
     postcss: wp => postcssProcessors(wp),
 };
