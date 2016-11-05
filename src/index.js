6f170824615aa9aad6ef5043d732b3c7cfb639eb
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-11-05 17:29:37 +0100

    Use middleware from react-router-redux
    - move history building in store/index
    - use routerMiddleware() from react-router-redux

--- src/index.js
+++ src/index.js
@@ -7,26 +7,14 @@ import './boot.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { useRouterHistory, match } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import createHashHistory from 'history/lib/createHashHistory';
-import createMemoryHistory from 'history/lib/createMemoryHistory';
-import Config from 'Config';
-import { isClientSide } from 'helpers/env';
-// import useScroll from 'scroll-behavior/lib/useScrollToTop';
+import { match } from 'react-router';
 
 // Styles
 import 'styles/_main.css';
 // Redux
-import { store } from './store';    // Redux store
+import { store, history } from './store';    // Redux store
 import Root from './root';          // App root (Router, Provider, Hot reload ...
 import routes from './routes';      // React-router routes
-
-// Build history
-const createHistory = isClientSide() ? createHashHistory : createMemoryHistory;
-// const browserHistory = useScroll(useRouterHistory(createHistory))();
-const browserHistory = useRouterHistory(createHistory)();
-const history = syncHistoryWithStore(browserHistory, store);
 
 // Init app function
 const loadApplication = (DOMElementId) => {
