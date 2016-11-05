fd1890df08e8c4a4e9bddf1f909c0470121a06de
Jalil Arfaoui <jalil@arfaoui.net>
2016-11-05 08:48:29 +0100

    Move bundle tasks to webpack.bundle.config.js
    Move HtmlWebpackPlugin, entry and output setting to webpack.bundle.config.js
    (not needed for storybook for example, which wants to use webpack.base.config.js)

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -1,31 +1,16 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const postcssProcessors = require('./postcss-processors');
 const loaders = require('./loaders');
 const resolve = require('./resolve');
 
-const CONSTANTS = require('./constants');
-const APP_PATH = CONSTANTS.APP_PATH;
-const APP_NAME = CONSTANTS.APP_NAME;
-const SRC_FOLDER = CONSTANTS.SRC_FOLDER;
-
 // Set-up common plugins
 const plugins = [];
 
 plugins.push(new webpack.NoErrorsPlugin()); // No assets emitted with errors
-plugins.push(new HtmlWebpackPlugin({
-    template: `${SRC_FOLDER}/index.html`,
-    inject: true,
-    filename: 'index.html',
-}));
 
 module.exports = {
     resolve,
-    entry: `${APP_PATH}/index.js`,
-    output: {
-        filename: `${APP_NAME}.[name].[hash].js`,
-    },
     plugins,
     module: { loaders },
     externals: {
