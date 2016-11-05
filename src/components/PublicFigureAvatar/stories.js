20a74e25fe3f023aaa910efac3e3dd9d5c3217a7
Jalil Arfaoui <jalil@arfaoui.net>
2016-11-05 09:23:44 +0100

    Add Storybook Knobs addon
    I add a completely useless usage example in PublicFigureAvatar/stories

--- src/components/PublicFigureAvatar/stories.js
+++ src/components/PublicFigureAvatar/stories.js
@@ -1,12 +1,17 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
 import PublicFigureAvatar from './';
 
-storiesOf('PublicFigureAvatar', module)
-    .add('pony avatar', () => (
-        <PublicFigureAvatar publicFigure={{ picture: { url: 'http://tinyurl.com/jucz8b9' } }} />
-    ))
-    .add('jake avatar', () => (
-        <PublicFigureAvatar publicFigure={{ picture: { url: 'http://tinyurl.com/jb286jq' } }} />
-    ));
+const stories = storiesOf('PublicFigureAvatar', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('pony avatar', () => (
+    <PublicFigureAvatar publicFigure={{ picture: { url: text('image url', 'http://tinyurl.com/jucz8b9') } }} />
+));
+
+stories.add('jake avatar', () => (
+    <PublicFigureAvatar publicFigure={{ picture: { url: 'http://tinyurl.com/jb286jq' } }} />
+));
