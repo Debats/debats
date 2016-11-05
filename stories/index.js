05d458eae1dd8bcf932fb23a227b0e6f4b511e1f
ato <jvincent@student.42.fr>
2016-11-05 22:05:59 +0100

    Update stories/index.js
    add Header stories and dep imports

--- stories/index.js
+++ stories/index.js
@@ -1,2 +1,12 @@
+import jquery from 'jquery';
+
+global.$ = jquery;
+global.jQuery = jquery;
+
+require('bootstrap-loader');
+
+import '../src/styles/typo.css';
+
 import '../src/components/general/Identity/stories.js';
 import '../src/components/PublicFigureAvatar/stories.js';
+import '../src/Main/Header/stories.js';
