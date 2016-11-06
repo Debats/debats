8e5075ce909aa549ccff5de0ffe800ca40f9d00e
ato <jvincent@student.42.fr>
2016-11-06 04:08:04 +0100

    Change Link for LinkContainer in Header
    use new react-router-bootstrap LinkContainer

--- src/Main/Header/index.jsx
+++ src/Main/Header/index.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import cssModules from 'react-css-modules';
+
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+
+import ConnectedUserMenu from './';
+
 import paths from 'constants/paths';
 import logoImg from './images/logo_header.png';
 import styles from './Header.css';
-import ConnectedUserMenu from './';
 
 const renderUserMenu = (isConnected) => (
-    isConnected
-    ? <ConnectedUserMenu />
-    : [
-        <li><Link to="#">Connexion</Link></li>,  /* modal ! */
-        <li><Link to="#">Inscription</Link></li>, /* modal ! */
-    ]
+  isConnected
+  ? <ConnectedUserMenu />
+  : [
+      <li><a>Connexion</a></li>,  /* modal ! */
+      <li><a>Inscription</a></li>, /* modal ! */
+  ]
 );
 
 const Header = () => (
-    <Navbar bsStyle="custom" fixedTop collapseOnSelect>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <Link to={paths.root} styleName="logo">
-                    <img src={logoImg} />
-                </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <Nav pullRight>
-                <NavItem href={paths.subjects}>Sujets</NavItem>
-                <NavItem href={paths.publicFigures}>Personnalités</NavItem>
-                <NavItem href={paths.manual}>Mode d'emploi</NavItem>
-                {renderUserMenu()}
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+  <Navbar bsStyle="custom" fixedTop collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <IndexLinkContainer to="/" styleName="logo">
+          <NavItem>
+            <img src={logoImg} />
+          </NavItem>
+        </IndexLinkContainer>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <LinkContainer to={{ pathname: paths.subjects, query: {} }}>
+          <NavItem>Sujets</NavItem>
+        </LinkContainer>
+        <LinkContainer to={{ pathname: paths.publicFigures, query: {} }}>
+          <NavItem>Personnalités</NavItem>
+        </LinkContainer>
+        <LinkContainer to={{ pathname: paths.manual, query: {} }}>
+          <NavItem>Mode d'emploi</NavItem>
+        </LinkContainer>
+        {renderUserMenu()}
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default cssModules(Header, styles);
