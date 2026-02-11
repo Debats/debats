fe6c1f19608764bd141d502b4667748179793bff
Jalil Arfaoui <jalil@arfaoui.net>
2026-02-11 23:12:14 +0100

    feat: ajouter le tracking Plausible Analytics via next-plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './global.css'
 import '../styles/debats-colors.css'
 import '../styles/layout.css'
+import PlausibleProvider from 'next-plausible'
 import { StyledComponentsRegistry } from './registry'
 import Header from '../components/layout/header'
 import Footer from '../components/layout/footer'
@@ -15,15 +16,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <body>
-        <StyledComponentsRegistry>
-          <div className="layout-container">
-            <Header />
-            <main className="main-content">
-              <div className="page-content">{children}</div>
-            </main>
-            <Footer />
-          </div>
-        </StyledComponentsRegistry>
+        <PlausibleProvider domain="debats.co">
+          <StyledComponentsRegistry>
+            <div className="layout-container">
+              <Header />
+              <main className="main-content">
+                <div className="page-content">{children}</div>
+              </main>
+              <Footer />
+            </div>
+          </StyledComponentsRegistry>
+        </PlausibleProvider>
       </body>
     </html>
   )
